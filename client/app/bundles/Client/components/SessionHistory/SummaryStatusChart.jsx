b86ffe1cc67ee4982d0c378c2099ee6c3572e268
loogle18 <sme@jetthoughts.com>
2017-10-09 00:26:29 +0300

    Aligned structure inside SummaryStatusChart.

--- client/app/bundles/Client/components/SessionHistory/SummaryStatusChart.jsx
+++ client/app/bundles/Client/components/SessionHistory/SummaryStatusChart.jsx
@@ -9,16 +9,16 @@ class SummaryStatusChart extends Component {
   render() {
     return(
       <BarChart width={1200} height={640} data={this.props.data}>
-       <XAxis dataKey='date'/>
-       <YAxis/>
-       <CartesianGrid strokeDasharray='3 3'/>
-       <Tooltip/>
-       <Legend/>
-       <Bar dataKey='passed' name='Passed' stackId='a' fill='#42c88a'/>
-       <Bar dataKey='stopped' name='Stopped' stackId='a' fill='#898989'/>
-       <Bar dataKey='failed' name='Failed' stackId='a' fill='#dd275a'/>
-       <Bar dataKey='error' name='Error' stackId='a' fill='#212121'/>
-       <Brush dataKey='date' height={25} stroke="#898989"/>
+        <XAxis dataKey='date'/>
+        <YAxis/>
+        <CartesianGrid strokeDasharray='3 3'/>
+        <Tooltip/>
+        <Legend/>
+        <Bar dataKey='passed' name='Passed' stackId='a' fill='#42c88a'/>
+        <Bar dataKey='stopped' name='Stopped' stackId='a' fill='#898989'/>
+        <Bar dataKey='failed' name='Failed' stackId='a' fill='#dd275a'/>
+        <Bar dataKey='error' name='Error' stackId='a' fill='#212121'/>
+        <Brush dataKey='date' height={25} stroke="#898989"/>
       </BarChart>
     );
   };
