5370de8b40d9417edb18f742b55cb3d4dab2a3ae
loogle18 <sme@jetthoughts.com>
2017-10-10 03:23:51 +0300

    Improved create_session_history_data_service.

--- app/services/create_session_history_data_service.rb
+++ app/services/create_session_history_data_service.rb
@@ -4,73 +4,42 @@ require 'csv'
 
 class CreateSessionHistoryDataService
   CSV_EXTENSION = '.csv'
-  CSV_SAMPLE_DATA_FILE = Rails.root.join('lib', 'sample_data', 'csv', 'session_history.csv').freeze
-  SESSION_ID_COLUMN_NAME = 'session_id'
-  DEFAULT_COLUMNS_MAP = {
-    session_id: 0,
-    started_by: 1,
-    created_at: 2,
-    summary_status: 3,
-    duration: 4,
-    worker_time: 5,
-    bundle_time: 6,
-    num_workers: 7,
-    branch: 8,
-    commit_id: 9
-  }.freeze
+  NEEDED_HEADERS = %i(session_id started_by created_at summary_status duration worker_time bundle_time
+                      num_workers branch commit_id).freeze
+  CSV_SAMPLE_DATA_FILE_PATH = Rails.root.join('lib', 'sample_data', 'csv', 'session_history.csv').freeze
 
-  attr_reader :csv_file
+  attr_reader :pathname
 
-  def initialize(csv_file = CSV_SAMPLE_DATA_FILE)
-    @csv_file = csv_file
+  def initialize(pathname = CSV_SAMPLE_DATA_FILE_PATH)
+    @pathname = pathname
   end
 
   def perform
     histories = []
 
-    return unless csv_file.exist? && File.extname(csv_file) == CSV_EXTENSION
+    return unless pathname.exist? && File.extname(pathname) == CSV_EXTENSION
 
-    opened_file = CSV.open(csv_file)
-    columns_map = get_columns_map_from(opened_file)
-
-    opened_file.readlines.each do |formatted_line|
-      histories << create_history(formatted_line, columns_map)
+    CSV.foreach(pathname, headers: true, header_converters: [:symbol]) do |row|
+      histories << create_history(row) if (row.headers & NEEDED_HEADERS).size == NEEDED_HEADERS.size
     end
-
-    opened_file.close
 
     histories
   end
 
   private
 
-  def get_columns_map_from(file)
-    first_line = file.first
-
-    # Check if it's line of columns' names. Presence of one of them is enough to be sure for most cases.
-    if first_line.include?(SESSION_ID_COLUMN_NAME)
-      order_map = {}
-      first_line.each_with_index { |field, index| order_map[field.to_sym] = index }
-      order_map
-    else
-      file.rewind # Restore to read from first line again.
-
-      DEFAULT_COLUMNS_MAP
-    end
-  end
-
-  def create_history(line, columns_map)
-    history = History.find_or_initialize_by(session_id: line[columns_map[:session_id]])
+  def create_history(row)
+    history = History.find_or_initialize_by(session_id: row[:session_id])
     history.update!(
-      started_by: line[columns_map[:started_by]],
-      created_at: line[columns_map[:created_at]],
-      summary_status: History::SUMMARY_STATUS_TO_CODE[line[columns_map[:summary_status]]],
-      duration: line[columns_map[:duration]],
-      worker_time: line[columns_map[:worker_time]],
-      bundle_time: line[columns_map[:bundle_time]],
-      num_workers: line[columns_map[:num_workers]],
-      branch: line[columns_map[:branch]],
-      commit_id: line[columns_map[:commit_id]]
+      started_by: row[:started_by],
+      created_at: row[:created_at],
+      summary_status: History::SUMMARY_STATUS_TO_CODE[row[:summary_status]],
+      duration: row[:duration],
+      worker_time: row[:worker_time],
+      bundle_time: row[:bundle_time],
+      num_workers: row[:num_workers],
+      branch: row[:branch],
+      commit_id: row[:commit_id]
     )
     history
   end
