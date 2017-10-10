3d6d4471f1d0f655707971bcc40c9d364c99cb16
loogle18 <sme@jetthoughts.com>
2017-10-10 05:54:33 +0300

    Updated NoMatch.

--- client/app/bundles/Client/components/NoMatch.jsx
+++ client/app/bundles/Client/components/NoMatch.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
+import css from './NoMatch.scss';
+
 class NoMatch extends Component {
   render() {
     return(
-      <div>
-        Requested page not found. Go to <a href='/'>home</a>
+      <div className={css.wrapper}>
+        Requested page not found. Go<a className={css.link} href='/'> home</a>
       </div>
-    )
-  }
-}
+    );
+  };
+};
 
 export default NoMatch;
